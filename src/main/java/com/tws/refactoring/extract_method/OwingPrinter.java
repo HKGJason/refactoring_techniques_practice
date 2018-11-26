5a5f05cfb916a230279e240c77406d55b9bbf62c
J <jason19951020@gmail.com>
2018-11-26 22:30:38 +0800

    refractor extract method

--- src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
+++ src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
@@ -8,22 +8,33 @@ import java.util.Vector;
 public class OwingPrinter {
     void printOwing(String name, List<Order> orders) {
         Iterator<Order> elements = orders.iterator();
-        double outstanding = 0.0;
 
-        // print banner
-        System.out.println ("*****************************");
-        System.out.println ("****** Customer totals ******");
-        System.out.println ("*****************************");
+        printBanner();
 
-        // print owings
-        while (elements.hasNext()) {
-            Order each = (Order) elements.next();
-            outstanding += each.getAmount();
-        }
+        double outstanding = getOutstanding(orders);
 
+        printDetails(name, outstanding);
+
+    }
+
+    private void printDetails(String name, double outstanding) {
         // print details
         System.out.println("name: " + name);
         System.out.println("amount: " + outstanding);
+    }
+
+    private void printBanner() {
+        System.out.println ("*****************************");
+        System.out.println ("****** Customer totals ******");
+        System.out.println ("*****************************");
+    }
+    private double getOutstanding(List<Order> orders){
+        double outstanding = 0.0;
+        while (orders.iterator().hasNext()){
+            Order order = (Order) orders.iterator().next();
+            outstanding += order.getAmount();
+        }
+        return outstanding;
     }
 }
 
